1b487810d1f287ab10835b0b2dedc80a9b9c4261
abdellah berrezoug <ab.berrezoug@esi-sba.dz>
2026-07-11 16:26:03 +0100

    ADD cas private

--- server/src/handlers/message.handler.ts
+++ server/src/handlers/message.handler.ts
@@ -1,7 +1,7 @@
 import { RawData, WebSocket } from "ws";
 import { addUser } from "../services/user.service.js";
 import { joinRoom, broadcastToRoom } from "../services/room.service.js";
-
+import { sendPrivateMessage } from "../services/private-message.service.js";
 export const handleMessage = (
   socket: WebSocket,
   data: RawData
@@ -48,7 +48,25 @@ export const handleMessage = (
 
         break;
       }
+case "private": {
+  console.log(
+    `📩 Message privé de ${payload.username} vers ${payload.to}`
+  );
 
+  const response = {
+    type: "private",
+    username: payload.username,
+    message: payload.message,
+    timestamp: new Date().toISOString(),
+  };
+
+  sendPrivateMessage(
+    payload.to,
+    JSON.stringify(response)
+  );
+
+  break;
+}
       default:
         console.log("❓ Type inconnu :", payload.type);
     }
