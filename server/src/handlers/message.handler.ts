3c05acadf5cf4d19135a3cb3820ccb6ee728f91c
abdellah berrezoug <ab.berrezoug@esi-sba.dz>
2026-07-11 15:52:31 +0100

    fix messageHandler

--- server/src/handlers/message.handler.ts
+++ server/src/handlers/message.handler.ts
@@ -1,11 +1,11 @@
 import { RawData, WebSocket } from "ws";
-import { addUser} from "../services/user.service.js";
-import { joinRoom } from "../services/room.service.js";
-;
+import { addUser } from "../services/user.service.js";
+import { joinRoom, broadcastToRoom } from "../services/room.service.js";
+
 export const handleMessage = (
   socket: WebSocket,
   data: RawData
-) => {
+): void => {
   const message = data.toString();
 
   console.log("📩 Message reçu :", message);
@@ -18,20 +18,41 @@ export const handleMessage = (
     switch (payload.type) {
       case "join":
         addUser(socket, payload.username);
-        console.log(`${payload.username} a rejoint le chat`);
+
+        console.log(`👤 ${payload.username} a rejoint le chat`);
         break;
 
-      case "message":
-        console.log(`${payload.username} : ${payload.message}`);
-        break;
       case "joinRoom":
         joinRoom(payload.room, socket);
-        console.log(`${payload.username} a rejoint le salon ${payload.room}`);
+
+        console.log(
+          `📁 ${payload.username} a rejoint le salon ${payload.room}`
+        );
         break;
+
+      case "message": {
+        console.log(`${payload.username} : ${payload.message}`);
+
+        const response = {
+          type: "message",
+          username: payload.username,
+          room: payload.room,
+          message: payload.message,
+          timestamp: new Date().toISOString(),
+        };
+
+        broadcastToRoom(
+          payload.room,
+          JSON.stringify(response)
+        );
+
+        break;
+      }
+
       default:
         console.log("❓ Type inconnu :", payload.type);
     }
   } catch (error) {
-    console.log("❌ JSON invalide");
+    console.error("❌ JSON invalide");
   }
 };
